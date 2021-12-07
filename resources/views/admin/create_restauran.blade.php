b61d58edcb58384f70d0d9d12c8f16d7dd008156
doducthuan <doducthuan29@gmail.com>
2021-12-07 20:30:01 +0700

    no merge

--- resources/views/admin/create_restauran.blade.php
+++ resources/views/admin/create_restauran.blade.php
@@ -37,15 +37,6 @@
     <form action="{{ route('restauran.store') }}" method="POST" enctype="multipart/form-data">
         @csrf
         <div class="form-group">
-            <label for="first_name">First Name:</label>
-            <input type="text" class="form-control" id="first_name" name="first_name">
-        </div>
-        <div class="form-group">
-            <label for="last_name">Last Name:</label>
-            <input type="text" class="form-control" id="last_name" name="last_name">
-        </div>
-
-        <div class="form-group">
             <label for="user_name">User Name:</label>
             <input type="text" class="form-control" id="user_name" name="user_name">
         </div>
